9dc6e6c69c5818ec1a675fab55af4f345e2e0642
Dmitriy Savinykh <biostif13@gmail.com>
2022-08-31 00:24:23 +0300

    Add calculation discount in Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,18 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val sumBye = 11_236_63
+    val coins = 100
+    val discount = 100
+    val discountPercent = 0.05
+    val discountRegular = 0.01
+    val regularClient = true
+    var check = sumBye
+
+    if (sumBye/coins > 1000) check = sumBye - discount * coins
+    if (sumBye/coins > 10000) check = (check - check * discountPercent).toInt()
+    if (regularClient) check = (check - check * discountRegular).toInt()
+
+    println("Чек на сумму: ${check/coins} руб. ${check%coins} коп.\nСкидка: "
+            + "${(sumBye - check)/coins} руб. ${(sumBye - check)%coins} коп.")
+
 }
